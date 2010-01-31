6ad9af9309da9f80c175edc5095287d47b939397
Sergey veged Berezhnoy <veged@mail.ru>
2010-01-31 04:49:36 +0300

    Работа с уровнями переопределений. Пример кастомного именования БЭМ в пределах уровня.

---
+++ Makefile
@@ -0,0 +1,2 @@
+test:
+	./bin/bembuild -d ../tests/decl.js -t css -t js -l ./tests/level1/ -l ./tests/level2/ -l ./tests/level3/
---
+++ tests/decl.js
@@ -0,0 +1,33 @@
+exports.blocks = [
+    {
+        name: 'first-block',
+        mods: [
+            {
+                name: 'mod1',
+                vals: ['1', '2']
+            },
+            {
+                name: 'mod2',
+                vals: ['3', '4']
+            }
+        ],
+        elems: [
+            {
+                name: 'elem1',
+                mods: [
+                    {
+                        name: 'mod1',
+                        vals: ['1', '2']
+                    },
+                    {
+                        name: 'mod2',
+                        vals: ['3', '4']
+                    }
+                ]
+            },
+            {
+                name: 'elem2'
+            }
+        ]
+    }
+];
