eda7011ef19558a7dc8fe9f5c4297de51ca6a4e5
Sergey veged Berezhnoy <veged@mail.ru>
2010-03-01 02:06:54 +0300

    Добавляем технологии к уровню переопределения при создании

--- tests/level4/.bem/level.js
+++ tests/level4/.bem/level.js
@@ -1,2 +1,9 @@
 var level = require('bem/level/simple');
 for (var n in level) exports[n] = level[n];
+exports.techs = {
+    'css': true,
+    'js': true
+};
+exports.techs = exports.techs || {};
+exports.techs['xsl'] = true;
+
--- tests/level5/.bem/level.js
+++ tests/level5/.bem/level.js
@@ -1,2 +1,7 @@
 var level = require('../../level2/.bem/level.js');
 for (var n in level) exports[n] = level[n];
+exports.techs = exports.techs || {};
+exports.techs['css'] = true;
+exports.techs['css1'] = true;
+delete exports.techs['js'];
+delete exports.techs['js1'];
