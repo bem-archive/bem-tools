f25771bd756743abea8c38962be060ac50651dcd
Sergey Belov <peimei@ya.ru>
2010-04-05 18:49:28 +0400

    bem create level:  - исправил ошибки в случае с несуществующей технологией  - исправил ошибку в обработке ключа -t (addTech)  - новый результат выполнения тестов

--- tests/level4/.bem/level.js
+++ tests/level4/.bem/level.js
@@ -1,9 +1,9 @@
 var level = require('bem/levels/simple');
 for (var n in level) exports[n] = level[n];
 exports.techs = {
-    'css': true,
-    'js': true
+    'css': 'bem/techs/css.js',
+    'js': ''
 };
 exports.techs = exports.techs || {};
-exports.techs['xsl'] = true;
+exports.techs['xsl'] = '';
 
--- tests/level5/.bem/level.js
+++ tests/level5/.bem/level.js
@@ -1,7 +1,7 @@
 var level = require('../../level2/.bem/level.js');
 for (var n in level) exports[n] = level[n];
 exports.techs = exports.techs || {};
-exports.techs['css'] = true;
-exports.techs['css1'] = true;
+exports.techs['css'] = 'bem/techs/css.js';
+exports.techs['css1'] = '';
 delete exports.techs['js'];
 delete exports.techs['js1'];
