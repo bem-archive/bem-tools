46a136a88570539f472b7add4d8e51efec9f207f
Sergey Belov <arikon@yandex-team.ru>
2011-10-15 23:44:13 +0400

    tests: Build with deps.js as declaration; canonize new tests results

---
+++ tests/bla.deps.js
@@ -0,0 +1,120 @@
+exports.deps = [
+    {
+        "block": "first-block"
+    },
+    {
+        "block": "dep-block"
+    },
+    {
+        "block": "first-block",
+        "elem": "elem1"
+    },
+    {
+        "block": "first-block",
+        "elem": "elem1",
+        "mod": "mod1"
+    },
+    {
+        "block": "first-block",
+        "elem": "elem1",
+        "mod": "mod1",
+        "val": "1"
+    },
+    {
+        "block": "first-block",
+        "elem": "elem1",
+        "mod": "mod1",
+        "val": "2"
+    },
+    {
+        "block": "first-block",
+        "elem": "elem1",
+        "mod": "mod2"
+    },
+    {
+        "block": "first-block",
+        "elem": "elem1",
+        "mod": "mod2",
+        "val": "3"
+    },
+    {
+        "block": "first-block",
+        "elem": "elem1",
+        "mod": "mod2",
+        "val": "4"
+    },
+    {
+        "block": "first-block",
+        "elem": "elem2"
+    },
+    {
+        "block": "first-block",
+        "mod": "mod1"
+    },
+    {
+        "block": "first-block",
+        "mod": "mod1",
+        "val": "1"
+    },
+    {
+        "block": "first-block",
+        "mod": "mod1",
+        "val": "2"
+    },
+    {
+        "block": "first-block",
+        "mod": "mod2"
+    },
+    {
+        "block": "first-block",
+        "mod": "mod2",
+        "val": "3"
+    },
+    {
+        "block": "first-block",
+        "mod": "mod2",
+        "val": "4"
+    },
+    {
+        "block": "second-block"
+    },
+    {
+        "block": "second-block",
+        "elem": "elem2"
+    },
+    {
+        "block": "second-block",
+        "elem": "elem2",
+        "mod": "mod22"
+    },
+    {
+        "block": "second-block",
+        "elem": "elem2",
+        "mod": "mod22",
+        "val": "221"
+    },
+    {
+        "block": "second-block",
+        "elem": "elem2",
+        "mod": "mod22",
+        "val": "222"
+    },
+    {
+        "block": "second-block",
+        "elem": "elem3"
+    },
+    {
+        "block": "second-block",
+        "mod": "mod2"
+    },
+    {
+        "block": "second-block",
+        "mod": "mod2",
+        "val": "21"
+    },
+    {
+        "block": "second-block",
+        "mod": "mod2",
+        "val": "22"
+    }
+];
