b54d2a69c1b8078bc6327027ce8a88ae9762a03e
Sergey Belov <arikon@yandex-team.ru>
2011-10-16 17:47:35 +0400

    Extend Tech class API, vows tests for Tech class, introduce OldTech class for backward compatibility with legacy tech modules
    Tech class API:
    
     - move createTech() static method to module level
    
     - add default value for techModule property
    
     - add support for multiple prefixes, add getPrefixes() method
    
     - rename getPathByPrefix() method to getPath() with semantic change
    
     - add getCreateResults() method, change semantics of getCreateResult()
    
     - add storeCreateResults() method, change semantics of storeCreateResult()
    
     - rename readPrefixContent() to readContent(), add readAllContent() method
    
     - add getBuildResults() method, change semantics of getBuildResult()
    
     - add storeBuildResults() method, change semantics of storeBuildResult()
    
     - new criteria of shortest path in getTechRelativePath() method: path components count instead of path length of string
    
    OldTech class
    
     - adapter to Tech class from 'old-tech' module
    
     - proxies create(), build(), readAllContent(), getTechName() and getTechPath() to equivalents
    
    Commands
    
     - add --force flag to 'bem create elem' and 'bem create mod'
    
    Tests
    
     - add tests to new Tech class
    
    Tech modules
    
     - refactor to use new Tech class
    
     - refactor of Deps and DepsItem classes to use 'inherit' module
    
    Package
    
     - add 'vows' to devDependencies

---
+++ test/tech-test.js
@@ -0,0 +1,109 @@
+var vows = require('vows'),
+    assert = require('assert'),
+    PATH = require('../lib/path'),
+    createTech = require('../lib/tech').createTech;
+
+function testBaseTech(techPath, techAlias) {
+    var batch = {},
+        techName = PATH.basename(techPath),
+        absTechPath = PATH.resolve(__dirname, techPath),
+        relTechPath = PATH.normalize(techPath);
+
+    if(/^\.\.\/lib\//.test(techPath)) {
+        absTechPath = relTechPath = techPath.replace(/^\.\.\/lib\//, 'bem/lib/');
+
+        // 'default' tech identified by '' relative path
+        if(techName == 'default') relTechPath = '';
+    }
+
+    techAlias = techAlias || techName;
+
+    batch["Tech.createTech('" + techPath + "')"] = {
+
+        topic: function() {
+            return createTech(require.resolve(techPath),
+                techAlias == techName ? null : techAlias);
+        },
+
+        // meta data
+        ".getTechName()": function(tech) {
+            assert.equal(tech.getTechName(), techAlias);
+        },
+
+        ".getSuffixes()" : {
+
+            topic: function(tech) {
+                return tech.getSuffixes();
+            },
+
+            "returns array": function(suffixes) {
+                assert.instanceOf(suffixes, Array);
+            },
+
+            "returns all suffixes": function(suffixes) {
+                assert.deepEqual(suffixes, [techAlias]);
+            }
+
+        },
+
+        ".matchSuffix()": function(tech) {
+            tech.getSuffixes().forEach(function(suffix) {
+                assert.isTrue(tech.matchSuffix(suffix));
+                assert.isTrue(tech.matchSuffix('.' + suffix));
+            });
+        },
+
+        ".getTechPath()": function(tech) {
+            assert.equal(tech.getTechPath(), absTechPath);
+        },
+
+        ".getTechRelativePath()": function(tech) {
+            assert.equal(tech.getTechRelativePath(__dirname), relTechPath);
+        },
+
+        // create
+        ".create()": {},
+
+        ".getCreateResult()": {},
+
+        ".getCreateResults()": function(tech) {
+            var res = tech.getCreateResults('test', { BlockName: 'b-test' });
+            tech.getSuffixes().forEach(function(suffix) {
+                assert.include(res, suffix);
+            });
+        },
+
+        ".storeCreateResult()": {},
+
+        ".storeCreateResults()": {},
+
+        ".readContent()": {},
+
+        ".readAllContent()": function(tech) {
+            var res = tech.readAllContent('test');
+            tech.getSuffixes().forEach(function(suffix) {
+                assert.include(res, suffix);
+            });
+        },
+
+        // build
+        ".build()": {},
+
+        ".getBuildResult()": {},
+
+        ".getBuildResults()": {},
+
+        ".storeBuildResult()": {},
+
+        ".storeBuildResults()": {}
+
+    };
+    return batch;
+}
+
+vows.describe('tech')
+    .addBatch(testBaseTech('../lib/techs/js'))
+    .addBatch(testBaseTech('../lib/techs/css'))
+    .addBatch(testBaseTech('../lib/techs/default', 'def'))
+    .addBatch(testBaseTech('./data/techs/test.js'))
+    .export(module);
