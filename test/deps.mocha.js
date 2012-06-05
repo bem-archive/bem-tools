ef5fd620646f5639260b2f6abfb9c404a07eb7a8
Sergey Belov <peimei@ya.ru>
2012-06-05 21:39:29 +0300

    Rewrite tests from vows to mocha: deps tests
    See bem/bem-tools#140

--- test/deps.mocha.js
+++ test/deps.mocha.js
@@ -1,33 +1,48 @@
-var vows = require('vows'),
-    assert = require('assert'),
-    DEPS = require('../lib/techs/deps.js'),
+var assert = require('chai').assert,
+    DEPS = require('../lib/techs/deps.js.js'),
     Deps = DEPS.Deps,
-    DepsItem = DEPS.DepsItem,
-    depsVows = vows.describe('Deps');
+    DepsItem = DEPS.DepsItem;
+
+/**
+ * Mocha BDD interface.
+ *
+ * @name describe @function
+ * @name it @function
+ * @name before @function
+ * @name after @function
+ * @name beforeEach @function
+ * @name afterEach @function
+ */
 
 function assertDepsParse(deps, expected) {
     return function () {
-        assert.deepEqual(new Deps().parse(deps).serialize(), expected)
-    }
+        assert.deepEqual(new Deps().parse(deps).serialize(), expected);
+    };
 }
 
-depsVows.addBatch({
+function assertBuildKey(item, expected) {
+    return function () {
+        assert.equal(new DepsItem(item).buildKey(), expected);
+    };
+}
 
-    'parsing:': {
+describe('Deps', function() {
 
-        'old format with names': {
+    describe('parsing:', function() {
 
-            'block': assertDepsParse(
+        describe('old format with names', function() {
+
+            it('block', assertDepsParse(
                 [ { name: 'b1' } ],
                 { '': { '': [ { block: 'b1' } ] } }
-            ),
+            ));
 
-            'block with elem': assertDepsParse(
+            it('block with elem', assertDepsParse(
                 [ { name: 'b1', elems: [ { name: 'e1' } ] } ],
                 { '': { '': [ { block: 'b1' }, { block: 'b1', elem: 'e1' } ] } }
-            ),
+            ));
 
-            'block with elem with mods with vals': assertDepsParse(
+            it('block with elem with mods with vals', assertDepsParse(
                 [
                     { name: 'b1', elems: [
                         { name: 'e1', mods: [
@@ -40,9 +55,9 @@ depsVows.addBatch({
                     { block: 'b1', elem: 'e1', mod: 'm1', val: 'v1' },
                     { block: 'b1', elem: 'e1', mod: 'm1', val: 'v2' }
                 ] } }
-            ),
+            ));
 
-            'block with mods with vals and with elems': assertDepsParse(
+            it('block with mods with vals and with elems', assertDepsParse(
                 [ { name: 'b1',
                     elems: [ 'e1', 'e2' ],
                     mods: [
@@ -57,45 +72,47 @@ depsVows.addBatch({
                     { block: 'b1', mod: 'm1', val: 'v1' },
                     { block: 'b1', mod: 'm2', val: 'v2' }
                 ] } }
-            )
+            ));
 
-        },
+        });
 
-        'new format': {
-            'block': assertDepsParse(
+        describe('new format', function() {
+
+            it('block', assertDepsParse(
                 [ { block: 'b1' } ],
                 { '': { '': [ { block: 'b1' } ] } }
-            ),
+            ));
 
-            'elem': assertDepsParse(
+            it('elem', assertDepsParse(
                 [ { block: 'b1', elem: 'e1' } ],
                 { '': { '': [ { block: 'b1', elem: 'e1' } ] } }
-            ),
+            ));
 
-            'block with shouldDeps and mustDeps': assertDepsParse(
+            it('block with shouldDeps and mustDeps', assertDepsParse(
                 [ { block: 'b1', shouldDeps: [ { block: 'b2', mustDeps: 'b3' }, 'b3' ] } ],
                 { '': { '': [ { block: 'b1' }, { block: 'b3' }, { block: 'b2' } ] } }
-            ),
+            ));
 
-            'simple blocks': assertDepsParse(
+            it('simple blocks', assertDepsParse(
                 [ 'b1', 'b2' ],
                 { '': { '': [ { block: 'b1' }, { block: 'b2' } ] } }
-            )
+            ));
 
-        },
+        });
 
-        'new format with techs': {
-            'block': assertDepsParse(
+        describe('new format with techs', function() {
+
+            it('block', assertDepsParse(
                 [ { tech: 't1', block: 'b1' } ],
                 { 't1': { 't1': [ { tech: 't1', block: 'b1' } ] } }
-            ),
+            ));
 
-            'elem': assertDepsParse(
+            it('elem', assertDepsParse(
                 [ { block: 'b1', elem: 'e1' } ],
                 { '': { '': [ { block: 'b1', elem: 'e1' } ] } }
-            ),
+            ));
 
-            'block with tech': assertDepsParse(
+            it('block with tech', assertDepsParse(
                 { block: 'b1', tech: 't1', shouldDeps: [ 'b2', 'b3' ], mustDeps: [ 'b0', 'b4' ] },
                 { 't1': { 't1': [
                     { block: 'b0', tech: 't1' },
@@ -104,124 +121,119 @@ depsVows.addBatch({
                     { block: 'b2', tech: 't1' },
                     { block: 'b3', tech: 't1' }
                 ] } }
-            ),
+            ));
 
-            'block with techs': assertDepsParse(
+            it('block with techs', assertDepsParse(
                 { block: 'b1', tech: 't1', shouldDeps: { block: 'b2', tech: 't2' } },
                 { 't1': {
                     't1': [ { block: 'b1', tech: 't1' } ],
                     't2': [ { block: 'b2', tech: 't2' } ]
                 } }
-            ),
+            ));
 
-            'block with techs': assertDepsParse(
+            it('block with and without tech', assertDepsParse(
                 { block: 'b1', shouldDeps: { block: 'b2', tech: 't2', shouldDeps: { block: 'b3' } } },
                 { '': {
                     '': [ { block: 'b1' } ],
                     't2': [ { block: 'b2', tech: 't2' }, { block: 'b3', tech: 't2' } ]
                 } }
-            )
-        },
+            ));
 
-        'noDeps': {
-            'block': assertDepsParse(
+        });
+
+        describe('noDeps', function() {
+
+            it('block', assertDepsParse(
                 [
                     { block: 'b1', shouldDeps: [ 'b2', 'b3' ], mustDeps: [ 'b0', 'b4' ] },
                     { block: 'b1', noDeps: ['b2', 'b4'] }
                 ],
                 { '': { '': [ { block: 'b0' }, { block: 'b1' }, { block: 'b3' } ] } }
-            )
-        }
+            ));
 
-    },
+        });
 
-    'clone': {
-        topic: function() {
-            var deps1 = new Deps().parse([{ block: 'b1', bla: 1 }, 'b2']),
-                deps2 = deps1.clone();
-            return [deps1, deps2];
-        },
+    });
 
-        '.items': function(deps) {
+    describe('clone', function() {
+
+        var deps1 = new Deps().parse([{ block: 'b1', bla: 1 }, 'b2']),
+            deps2 = deps1.clone(),
+            deps = [deps1, deps2];
+
+        it('.items', function() {
             assert.deepEqual(deps[1].items, deps[0].items)
-        },
+        });
 
-        '.itemsByOrder': function(deps) {
+        it('.itemsByOrder', function() {
             assert.deepEqual(deps[1].itemsByOrder, deps[0].itemsByOrder)
-        }
-    }
+        });
+
+    });
+
+
+    describe('DepsItem', function() {
+
+        describe('buildKey', function() {
+
+            it('block without tech', assertBuildKey(
+                { block: 'b1' },
+                'b1'));
+
+            it('block modifier without tech', assertBuildKey(
+                { block: 'b1', mod: 'm1' },
+                'b1_m1'));
+
+            it('block modifier value without tech', assertBuildKey(
+                { block: 'b1', mod: 'm1', val: 'v1' },
+                'b1_m1_v1'));
+
+            it('block element without tech', assertBuildKey(
+                { block: 'b1', elem: 'e1' },
+                'b1__e1'));
+
+            it('element modifier without tech', assertBuildKey(
+                { block: 'b1', elem: 'e1', mod: 'm1' },
+                'b1__e1_m1'));
+
+            it('element modifier value without tech', assertBuildKey(
+                { block: 'b1', elem: 'e1', mod: 'm1', val: 'v1' },
+                'b1__e1_m1_v1'));
+
+            it('block with tech', assertBuildKey(
+                { tech: 't1', block: 'b1' },
+                'b1.t1'));
+
+            it('block modifier with tech', assertBuildKey(
+                { tech: 't1', block: 'b1', mod: 'm1' },
+                'b1_m1.t1'));
+
+            it('block modifier value with tech', assertBuildKey(
+                { tech: 't1', block: 'b1', mod: 'm1', val: 'v1' },
+                'b1_m1_v1.t1'));
+
+            it('block element with tech', assertBuildKey(
+                { tech: 't1', block: 'b1', elem: 'e1' },
+                'b1__e1.t1'));
+
+            it('element modifier with tech', assertBuildKey(
+                { tech: 't1', block: 'b1', elem: 'e1', mod: 'm1' },
+                'b1__e1_m1.t1'));
+
+            it('element modifier value with tech', assertBuildKey(
+                { tech: 't1', block: 'b1', elem: 'e1', mod: 'm1', val: 'v1' },
+                'b1__e1_m1_v1.t1'));
+
+            it('just tech', assertBuildKey(
+                { tech: 't1' },
+                '.t1'));
+
+            it('empty', assertBuildKey(
+                {},
+                ''));
+
+        });
+
+    });
 
 });
-
-
-function assertBuildKey(item, expected) {
-    return function () {
-        assert.equal(new DepsItem(item).buildKey(), expected)
-    }
-}
-
-depsVows.addBatch({
-
-    'DepsItem': {
-        'buildKey': {
-
-            'block without tech': assertBuildKey(
-                { block: 'b1' },
-                'b1'),
-
-            'block modifier without tech': assertBuildKey(
-                { block: 'b1', mod: 'm1' },
-                'b1_m1'),
-
-            'block modifier value without tech': assertBuildKey(
-                { block: 'b1', mod: 'm1', val: 'v1' },
-                'b1_m1_v1'),
-
-            'block element without tech': assertBuildKey(
-                { block: 'b1', elem: 'e1' },
-                'b1__e1'),
-
-            'element modifier without tech': assertBuildKey(
-                { block: 'b1', elem: 'e1', mod: 'm1' },
-                'b1__e1_m1'),
-
-            'element modifier value without tech': assertBuildKey(
-                { block: 'b1', elem: 'e1', mod: 'm1', val: 'v1' },
-                'b1__e1_m1_v1'),
-
-            'block with tech': assertBuildKey(
-                { tech: 't1', block: 'b1' },
-                'b1.t1'),
-
-            'block modifier with tech': assertBuildKey(
-                { tech: 't1', block: 'b1', mod: 'm1' },
-                'b1_m1.t1'),
-
-            'block modifier value with tech': assertBuildKey(
-                { tech: 't1', block: 'b1', mod: 'm1', val: 'v1' },
-                'b1_m1_v1.t1'),
-
-            'block element with tech': assertBuildKey(
-                { tech: 't1', block: 'b1', elem: 'e1' },
-                'b1__e1.t1'),
-
-            'element modifier with tech': assertBuildKey(
-                { tech: 't1', block: 'b1', elem: 'e1', mod: 'm1' },
-                'b1__e1_m1.t1'),
-
-            'element modifier value with tech': assertBuildKey(
-                { tech: 't1', block: 'b1', elem: 'e1', mod: 'm1', val: 'v1' },
-                'b1__e1_m1_v1.t1'),
-
-            'just tech': assertBuildKey(
-                { tech: 't1' },
-                '.t1'),
-
-            'empty': assertBuildKey(
-                {},
-                '')
-
-        }
-    }
-
-}).export(module);
