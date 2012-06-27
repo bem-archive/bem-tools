6bbd8502bde24e522a07a82ab934f2b0b1366e01
Sergey Belov <peimei@ya.ru>
2012-06-27 16:17:39 +0300

    Use Tech.getCreateSuffixes() and Tech.getBuildSuffixes() all over the base Tech class

--- test/data/make/project/bem-bl/blocks-common/i-bem/bem/techs/bemhtml.js
+++ test/data/make/project/bem-bl/blocks-common/i-bem/bem/techs/bemhtml.js
@@ -86,14 +86,6 @@ exports.getBuildResult = function(prefixes, suffix, outputDir, outputName) {
 
 };
 
-exports.storeBuildResults = function(prefix, res) {
-    var _this = this,
-        suffix = 'bemhtml.js';
-    return Q.when(res, function(res) {
-        return _this.storeBuildResult(_this.getPath(prefix, suffix), suffix, res['bemhtml']);
-    });
-};
-
 exports.getSuffixes = function() {
     return ['bemhtml'];
 };
