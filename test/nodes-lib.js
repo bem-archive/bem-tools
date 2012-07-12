613bd568b9d585c20ab69e53d42ffb5395d958a3
Vladimir Alaev <scff@yandex-team.ru>
2012-07-12 17:01:08 +0300

    - a bit of refactoring

--- test/nodes-lib.js
+++ test/nodes-lib.js
@@ -3,7 +3,7 @@ var assert = require('chai').assert,
     PATH = require('path'),
     QFS = require('q-fs'),
 
-    SymlinkLibraryNode = require('../lib/nodes/lib');
+    LibNodes = require('../lib/nodes/lib');
 
 
 /**
@@ -21,7 +21,7 @@ describe('nodes', function() {
 
     describe('new SymlinkLibraryNode()', function() {
 
-        var node = new SymlinkLibraryNode.SymlinkLibraryNode({
+        var node = new LibNodes.SymlinkLibraryNode({
                 root: PATH.resolve(__dirname, 'data'),
                 target: 'symlink',
                 relative: 'lib'
