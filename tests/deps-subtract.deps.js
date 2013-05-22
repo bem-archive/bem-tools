19d84c0a82f74843a51387b891add51a62339410
Sergey Belov <peimei@ya.ru>
2013-05-23 00:18:39 +0300

    Canonize legacy `make tests` tests results for `bem build -t deps.js`, `bem decl merge` and `bem decl subtract` commands

--- tests/deps-subtract.deps.js
+++ tests/deps-subtract.deps.js
@@ -7,6 +7,32 @@ exports.deps = [
         "elem": "e2"
     }
 ];
-exports.depsByTechs = {
-    "": {}
+exports.depsFull = {
+    "": {
+        "shouldDeps": [
+            "b1",
+            "b1__e1",
+            "b1__e2"
+        ],
+        "mustDeps": [],
+        "item": {},
+        "key": ""
+    },
+    "b1": {
+        "shouldDeps": [],
+        "mustDeps": [],
+        "item": {
+            "block": "b1"
+        },
+        "key": "b1"
+    },
+    "b1__e2": {
+        "shouldDeps": [],
+        "mustDeps": [],
+        "item": {
+            "block": "b1",
+            "elem": "e2"
+        },
+        "key": "b1__e2"
+    }
 };
