a6b920a99361f05a654064653220034fda41b935
Serhii Tatarintsev <sevinf@yandex-team.ru>
2013-08-01 17:31:20 +0300

    Tests for js-i tech

---
+++ test/js-i-tech.js
@@ -0,0 +1,38 @@
+'use strict';
+var BEMTesting = require('../lib/testing');
+
+describe('js-i tech', function() {
+    var tech;
+    beforeEach(function() {
+        tech = BEMTesting.testTech(require.resolve('../lib/techs/v2/js-i'));
+    });
+
+    describe('building', function() {
+        it('produces js file with includes for all sources', function(done) {
+            tech.withSourceFiles({
+                'menu': {
+                    'menu.js': '',
+                    '__item': {
+                        'menu__item.js': ''
+                    }
+                },
+                'logo': {
+                    'logo.js': ''
+                }
+            })
+            .build('/out', {
+                deps: [
+                    {block: 'menu'},
+                    {block: 'menu', elem: 'item'},
+                    {block: 'logo'},
+                ]
+            })
+            .producesFile('/out.js')
+            .withContent('/*borschik:include:menu/menu.js*/;',
+                         '/*borschik:include:menu/__item/menu__item.js*/;',
+                         '/*borschik:include:logo/logo.js*/;',
+                         '')
+            .notify(done);
+        });
+    });
+});
