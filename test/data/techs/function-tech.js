62071408377aea7867f52134c477bef48d27720b
Vladimir Alaev <scff@yandex-team.ru>
2013-08-19 17:18:43 +0300

    Add ability to write tech module as a function (closes #363, BEM-930)

---
+++ test/data/techs/function-tech.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = function(BEM) {
+    return {
+        techMixin: {
+
+            getBuildResults: function() {
+                return BEM.require.resolve('./techs/js');
+            }
+        }
+    };
+};
