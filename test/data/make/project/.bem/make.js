b6a8453e98477195e89562ad112abc115e07fda3
Vladimir Alaev <scff@yandex-team.ru>
2013-08-19 15:40:40 +0300

    Make .bem/make.js config to have a requirable format and exporting a function with MAKE argument (BEM-928)

--- test/data/make/project/.bem/make.js
+++ test/data/make/project/.bem/make.js
@@ -1,66 +1,69 @@
 var PATH = require('path');
 
-MAKE.decl('Arch', {
+module.exports = function(MAKE) {
 
-    getLevelCachePolicy: function() {
-        return {
-            cache: false,
-            except: [
-                'bem-bl/blocks-common',
-                'bem-bl/blocks-desktop'
-            ]
+    MAKE.decl('Arch', {
+
+        getLevelCachePolicy: function() {
+            return {
+                cache: false,
+                except: [
+                    'bem-bl/blocks-common',
+                    'bem-bl/blocks-desktop'
+                ]
+            }
         }
-    }
 
-});
+    });
 
-// Build i18n files
-MAKE.decl('BundleNode', {
+    // Build i18n files
+    MAKE.decl('BundleNode', {
 
-    getTechs: function() {
+        getTechs: function() {
 
-        var arr = this.__base();
+            var arr = this.__base();
 
-        // remove js tech
-        arr.splice(arr.indexOf('js'), 1);
+            // remove js tech
+            arr.splice(arr.indexOf('js'), 1);
 
-        // add i18n techs
-        return arr.concat(['i18n', 'i18n.js']);
+            // add i18n techs
+            return arr.concat(['i18n', 'i18n.js']);
 
-    },
+        },
 
-    'create-i18n.js-optimizer-node': function(tech, sourceNode, bundleNode) {
+        'create-i18n.js-optimizer-node': function(tech, sourceNode, bundleNode) {
 
-        sourceNode.getFiles().forEach(function(f) {
-            this['create-js-optimizer-node'](tech, this.ctx.arch.getNode(f), bundleNode);
-        }, this);
+            sourceNode.getFiles().forEach(function(f) {
+                this['create-js-optimizer-node'](tech, this.ctx.arch.getNode(f), bundleNode);
+            }, this);
 
-    }
+        }
 
-});
-
-
-// Build merged bundle
-MAKE.decl('BundleNode', {
-
-    getTechs: function() {
-
-        if (this.getLevelPath() === 'pages-with-merged') return [
-            'bemdecl.js',
-            'deps.js'
-        ];
-
-        return this.__base();
-
-    }
-
-});
+    });
 
 
-MAKE.decl('BundlesLevelNode', {
+    // Build merged bundle
+    MAKE.decl('BundleNode', {
 
-    buildMergedBundle: function() {
-        return this.getLevelPath() === 'pages-with-merged';
-    }
+        getTechs: function() {
 
-});
+            if (this.getLevelPath() === 'pages-with-merged') return [
+                'bemdecl.js',
+                'deps.js'
+            ];
+
+            return this.__base();
+
+        }
+
+    });
+
+
+    MAKE.decl('BundlesLevelNode', {
+
+        buildMergedBundle: function() {
+            return this.getLevelPath() === 'pages-with-merged';
+        }
+
+    });
+}
