d93d204d78f0c102285df05e87d6d9058cda2c9c
Vladimir Alaev <scff@yandex-team.ru>
2013-08-19 18:18:21 +0300

    Add ability to write level config module as a function (closes #364, BEM-931)

--- test/data/make/project/pages/.bem/level.js
+++ test/data/make/project/pages/.bem/level.js
@@ -1,36 +1,37 @@
-var BEM = require('../../..'),
-    PATH = require('path'),
-    extend = BEM.util.extend,
+module.exports = function(BEM) {
 
-    BEM_TECHS = '../../bem-bl/blocks-common/i-bem/bem/techs',
-    PRJ_TECHS = '../../.bem/techs';
+    var PATH = require('path'),
+        extend = BEM.util.extend,
 
-exports.getTechs = function() {
+        BEM_TECHS = '../../bem-bl/blocks-common/i-bem/bem/techs';
 
     return {
-        'bemjson.js': '',
-        'deps.js': 'v2/deps.js',
-        'js': 'v2/js-i',
-        'css': 'v2/css',
-        'ie.css': 'v2/ie.css',
-        'i18n': PATH.join(BEM_TECHS, 'v2/i18n.js'),
-        'i18n.js': PATH.join(BEM_TECHS, 'v2/i18n.js.js'),
-        'bemhtml': PATH.join(BEM_TECHS, 'v2/bemhtml.js'),
-        'html': PATH.join(BEM_TECHS, 'html.js')
+        getTechs: function() {
+
+            return {
+                'bemjson.js': '',
+                'deps.js': 'v2/deps.js',
+                'js': 'v2/js-i',
+                'css': 'v2/css',
+                'ie.css': 'v2/ie.css',
+                'i18n': PATH.join(BEM_TECHS, 'v2/i18n.js'),
+                'i18n.js': PATH.join(BEM_TECHS, 'v2/i18n.js.js'),
+                'bemhtml': PATH.join(BEM_TECHS, 'v2/bemhtml.js'),
+                'html': PATH.join(BEM_TECHS, 'html.js')
+            };
+        },
+
+        getConfig: function() {
+
+            return extend({}, this.__base() || {}, {
+
+                bundleBuildLevels: this.resolvePaths([
+                    '../../bem-bl/blocks-common',
+                    '../../bem-bl/blocks-desktop',
+                    '../../blocks'
+                ])
+
+            });
+        }
     };
-
-};
-
-exports.getConfig = function() {
-
-    return extend({}, this.__base() || {}, {
-
-        bundleBuildLevels: this.resolvePaths([
-            '../../bem-bl/blocks-common',
-            '../../bem-bl/blocks-desktop',
-            '../../blocks'
-        ])
-
-    });
-
 };
